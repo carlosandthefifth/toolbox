de778457e78f49a0b8ef6b35d62df0593a8a4ca7
gdansfdev@gmail.com <dgustafson@dgustafson-ltm4.internal.salesforce.com>
2019-08-29 14:02:52 -0700

    adding progress interface

--- factoryData/force-app/main/default/aura/objects/objectsController.js
+++ factoryData/force-app/main/default/aura/objects/objectsController.js
@@ -1,18 +1,28 @@
 ({
     doInit : function(component, event, helper) {
         var action = component.get("c.getObjectNames");
-
         action.setCallback(this,function(response){
             var values = response.getReturnValue()
             console.error("values: " + values);
             component.set("v.objectNames", values);
         });
         $A.enqueueAction(action);
+        component.set('v.showLoading', false);
+        $A.get('e.force:refreshView').fire();
     },
 
     configureObject : function(component, event, helper) {
         console.error("in here");
         var objectButton = event.getSource().get("v.label");
         alert(objectButton + " was pressed");
+    },
+
+    objectDetails : function(component, event, helper) {
+        component.set("v.showIt", true);
+        $A.get('e.force:refreshView').fire();
+    },
+
+    isRefreshed : function(component, event, helper) {
+
     }
 })
