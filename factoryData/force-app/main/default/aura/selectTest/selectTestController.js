0b6bae90f3c4bd480b770ae5e10d1e608de20694
gcarloscodeview <gdansfdev@gmail.com>
2019-09-03 06:11:40 -0700

    moved debug code into functions  readability

--- factoryData/force-app/main/default/aura/selectTest/selectTestController.js
+++ factoryData/force-app/main/default/aura/selectTest/selectTestController.js
@@ -1,25 +1,6 @@
 ({
     doInit: function (component, event, helper) {
-        console.info("inside doinit");
-        var action = component.get("c.discoverObjects");
-
-        action.setCallback(this,function(response){
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                console.error("response.getReturnValue(): " +  response.getReturnValue());
-                var returnValues = response.getReturnValue();
-                component.set("v.optionMap", returnValues);
-                console.log("returnValues: " + returnValues);
-                var optionValues = [];
-                for (var singlekey in returnValues) {
-                    optionValues.push({label: singlekey + ":" + returnValues[singlekey], value: singlekey});
-                }
-                console.log("optionValues: " + optionValues);
-                component.set("v.options", optionValues);
-                
-            }
-        });
-        $A.enqueueAction(action);
+        helper.doInit(component,event);
     },
 
     cancel: function(component, event, helper) {
@@ -27,76 +8,24 @@
     },
 
     changeRecordCount: function(component, event, helper) {
-        component.set("v.showEditRecordCount", true);
-        var selectedOptions = component.get("v.selectedOptions");
-        console.warn("options2: " + selectedOptions);
-
-        var mapSelection = component.get("v.optionMap");
-        console.error("mapSelection: " + JSON.stringify(mapSelection));
-        var strArray = [];
-        for (var i = 0 ; i < selectedOptions.length; i++) {
-            console.log("selectedOptions[i]: " + selectedOptions[i]);
-            strArray.push(selectedOptions[i]);
-            strArray.push(mapSelection[selectedOptions[i]]);
-        }
-
-        for (var i = 0; i < strArray.length; i++) {
-            console.log("&&& strArray: " + strArray[i]);
-        }
-        console.log("StrArray: " + strArray);
-        component.set("v.objectRecords", strArray);
+        helper.changeRecordCount(component,event);
     },
 
     createData: function(component, event, helper) {
 
     },
-    
+
     doRefresh: function(component, event, helper) {
 
     },
 
     save: function(component, event, helper) {
-        var myInputs = component.find("myDiv").find({instancesOf : "lightning:input"});
-        alert("There are " + myInputs.length + " lightning:input elements within myDiv.");
-
-        var optionsSelected = component.get('v.selectedOptions');
-        var optionMap = component.get("v.optionMap");
-        for(var i = 0; i < myInputs.length; i++) {
-            console.log("i: " + i);
-            console.log("------> aura element in array value @ index " + i + " = " + myInputs[i].get("v.value"));
-            console.log("------> aura element in array name @ index " + i + " = " + myInputs[i].get("v.name"));
-            console.log("optionsSelected[" + i + "]: " + optionsSelected[i]);
-            console.log("optionMap[optionsSelected[i]]: " + optionMap[optionsSelected[i]]);
-            optionMap[optionsSelected[i]] = myInputs[i].get("v.value");
-        }
-        
-        
-
-        
-        console.log("optionMap: " + JSON.stringify(optionMap));
-        var optionValues = [];
-        for (var singlekey in optionMap) {
-            optionValues.push({label: singlekey + ":" + optionMap[singlekey], value: singlekey});
-        }
-        console.log("optionValues: " + optionValues);
-        component.set("v.options", optionValues);
-        component.set("v.showEditRecordCount",false);
-        $A.get("e.force:refreshView").fire();
+        helper.save(component,event);
     },
     
 
 
-    onClick: function(component, event, helper) {
-        var whodis = event.getSource().getLocalId();
-        console.info("whodis: " + whodis);
-        var selectedOptionsList = event.getParam("value");
-        component.set("v.selectedOptions", selectedOptionsList);
-        console.info("selectedOptionsList: " + selectedOptionsList);
-        console.info("selectedOptionsList.length: " + selectedOptionsList.length);
-        if ((selectedOptionsList.length > 0))
-            component.set("v.showChangeRecordButton", true)
-        else    
-            component.set("v.showChangeRecordButton", false)
-        $A.get("e.force:refreshView").fire();
+    onSelection: function(component, event, helper) {
+        helper.onClick(component,event,helper);
     }
 });
--- factoryData/force-app/main/default/aura/selectTest/selectTestHelper.js
+++ factoryData/force-app/main/default/aura/selectTest/selectTestHelper.js
@@ -1,5 +1,138 @@
 ({
-    helperMethod : function() {
+    doInit: function (component, event) {
+        console.info("inside doinit");
+        var action = component.get("c.discoverObjects");
 
+        action.setCallback(this,function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var returnValues = response.getReturnValue();
+                component.set("v.optionMap", returnValues);
+                var optionValues = [];
+                for (var singlekey in returnValues) {
+                    optionValues.push({label: singlekey + ":" + returnValues[singlekey], value: singlekey});
+                }
+                component.set("v.options", optionValues);
+
+                
+                // Are we in debug mode    
+                var debugMode = component.get("v.debugModeOn");
+                if (debugMode) // output to console
+                    helper.debug_doInit(returnValues, optionValues);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    debug_doInit: function(returnValues, optionValues) {
+        console.log("returnValues: " + returnValues);
+        console.log("optionValues: " + optionValues);
+    },
+
+    cancel: function(component, event) {
+
+    },
+
+    changeRecordCount: function(component, event, helper) {
+        component.set("v.showEditRecordCount", true);
+        var selectedOptions = component.get("v.selectedOptions");
+       
+
+        var mapSelection = component.get("v.optionMap");
+        
+        var strArray = [];
+        for (var i = 0 ; i < selectedOptions.length; i++) {       
+            strArray.push(selectedOptions[i]);
+            strArray.push(mapSelection[selectedOptions[i]]);
+        }
+
+        component.set("v.objectRecords", strArray);
+        var debugMode = component.get("v.debugModeOn");
+        if (debugMode) // output to console
+            helper.debug_changeRecordCount(selectedOptions, mapSelection, strArray);
+    },
+
+    debug_changeRecordCount: function(selectedOptions, mapSelection, strArray) {
+        console.warn("options2: " + selectedOptions);
+        console.error("mapSelection: " + JSON.stringify(mapSelection));
+        console.log("selectedOptions[i]: " + selectedOptions[i]);
+        for (var i = 0; i < strArray.length; i++) {
+            console.log("&&& strArray: " + strArray[i]);
+        }
+        console.log("StrArray: " + strArray);
+    },
+
+    createData: function(component, event) {
+
+    },
+    
+    doRefresh: function(component, event) {
+
+    },
+
+    save: function(component, event) {
+        // Get the number of instances of Lighting:Inputs in this div
+        var myInputs = component.find("myDiv").find({instancesOf : "lightning:input"});
+        var optionsSelected = component.get('v.selectedOptions'); // Get the objects and new record count
+        var optionMap = component.get("v.optionMap"); // Get the map that contains all objects and record count
+        
+        var debugMode = component.get("v.debugModeOn");
+        // Are we in debug mode?
+        for(var i = 0; i < myInputs.length; i++) {
+    
+            optionMap[optionsSelected[i]] = myInputs[i].get("v.value");
+        }
+        
+        var optionValues = [];
+        for (var singlekey in optionMap) {
+            optionValues.push({label: singlekey + ":" + optionMap[singlekey], value: singlekey});
+        }
+        
+        component.set("v.options", optionValues);
+        component.set("v.showEditRecordCount",false);
+        $A.get("e.force:refreshView").fire();
+
+        if (debugMode) // output to console
+        helper.debug_save(myInputs,optionsSelected,optionMap,optionValues);
+    },
+    
+    debug_save: function(myInputs,optionsSelected,optionMap,optionValues) {
+        alert("There are " + myInputs.length + " lightning:input elements within myDiv.");
+        for(var i = 0; i < myInputs.length; i++) {
+            console.log("i: " + i);
+            console.log("------> aura element in array value @ index " + i + " = " + myInputs[i].get("v.value"));
+            console.log("------> aura element in array name @ index " + i + " = " + myInputs[i].get("v.name"));
+            console.log("optionsSelected[" + i + "]: " + optionsSelected[i]);
+            console.log("optionMap[optionsSelected[i]]: " + optionMap[optionsSelected[i]]);
+        }
+        console.log("optionMap: " + JSON.stringify(optionMap));
+        console.log("optionValues: " + optionValues);
+    },
+
+    onSelection: function(component, event, helper) {
+
+        var selectedOptionsList = event.getParam("value"); // What have we selected if anything
+
+        component.set("v.selectedOptions", selectedOptionsList); // Add the selected item to the select options list
+        
+        // Do we have items selected
+        if ((selectedOptionsList.length > 0))
+            component.set("v.showChangeRecordButton", true) // show the button that allows updating the record count
+        else    
+            component.set("v.showChangeRecordButton", false) // hide the button that allows updating the record count
+        // Refresh page    
+        $A.get("e.force:refreshView").fire();
+
+        // Are we in debug mode    
+        var debugMode = component.get("v.debugModeOn");
+        if (debugMode) // output to console
+            helper.debug_onSelection(event,selectedOptionsList);
+    },
+
+    debug_onSelection: function(event,selectedOptionsList) {
+        var whodis = event.getSource().getLocalId();
+        console.info("whodis: " + whodis);
+        console.info("selectedOptionsList: " + selectedOptionsList);
+        console.info("selectedOptionsList.length: " + selectedOptionsList.length);
     }
 })
